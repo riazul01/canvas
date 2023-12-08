b8babd904a9c4b98fb0481b0de7b8f32c15641e7
Riazul Islam <riazul5207@gmail.com>
2023-12-08 21:35:07 +0600

    sidebar close functionality added

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+const sidebar = document.querySelector('.sidebar');
+const sidebarCloseBtn = document.querySelector('.close-sidebar');
+
+sidebarCloseBtn.onclick = () => {
+    sidebar.classList.remove('active');
+}
