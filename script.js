86c084e3ded9cf12896817ed368c4d80105040ee
Riazul Islam <riazul5207@gmail.com>
2023-12-16 18:35:52 +0600

    settings ui updated

--- script.js
+++ script.js
@@ -14,11 +14,15 @@ const body = document.querySelector('body');
 // toggle switch box
 toggleSettings.addEventListener('click', () => {
     settings.classList.toggle('show-settings');
+    toggleSettings.classList.toggle('remove');
 });
 
 // close switch box
 body.addEventListener('click', () => {
     settings.classList.remove('show-settings');
+    setTimeout(() => {
+        toggleSettings.classList.remove('remove');
+    }, 200);
 });
 
 settings.addEventListener('click', (e) => {
