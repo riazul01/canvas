5a62148374c9f7a408e00b0bd07eaa01ab691dbd
Riazul Islam <riazul5207@gmail.com>
2023-12-16 12:07:10 +0600

    app settings ui completed

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const sidebarCloseBtn = document.querySelector('.close-sidebar');
 sidebarCloseBtn.onclick = () => {
     sidebar.classList.remove('active');
 }
+
+// settings
+const settings = document.querySelector('.settings');
+const toggleSettings = document.querySelector('.toggle-settings');
+
+const body = document.querySelector('body');
+
+// toggle switch box
+toggleSettings.addEventListener('click', () => {
+    settings.classList.toggle('show-settings');
+});
+
+// close switch box
+body.addEventListener('click', () => {
+    settings.classList.remove('show-settings');
+});
+
+settings.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
